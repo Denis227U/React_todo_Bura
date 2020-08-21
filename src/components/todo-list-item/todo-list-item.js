9be7cefb1decb223485c9bd56f19f35a5c90f825
Denis Yunyaev <music_is_my_extazy@mail.ru>
2020-08-22 00:53:29 +0300

    lesson 35 done

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -2,6 +2,19 @@ import React from "react";
 import "./todo-list-item.css";
 
 export default class TodoListItem extends React.Component {
+  constructor() {
+    super();
+
+    this.onLabelClick = () => {
+      console.log(`Done: ${this.props.label}`);
+    };
+  }
+
+  // 1вариант с bind
+  // onLabelClick() {
+  //   console.log(`Done: ${this.props.label}`);
+  // }
+
   render() {
     // this.props; то место откуда можно получить текущее свойство
     const { label, important = false } = this.props;
@@ -13,7 +26,13 @@ export default class TodoListItem extends React.Component {
 
     return (
       <span className="todo-list-item d-flex align-items-center justify-content-between">
-        <span style={style} className="todo-list-item-label">
+        <span
+          style={style}
+          className="todo-list-item-label"
+          // 1вариант с bind
+          // onClick={this.onLabelClick.bind(this)}
+          onClick={this.onLabelClick}
+        >
           {label}
         </span>
 
